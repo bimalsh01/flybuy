7d6103431ceb99b8750a24f9cdc339b10e1671f7
Bimal Shrestha <bimalstha291@gmail.com>
2023-06-22 08:54:56 +0545

    upto register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+
+  const [firstname, setFirstname] = useState('')
+  cosnt [lastname, setLastname] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confpassword, setConfpassword] = useState('')
+
+  const handleChangeFirstname = (e) => {
+    setFirstname(e.target.value)
+  }
+
+  const handleChangeLastname = (e) => {
+    setLastname(e.target.value)
+  }
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleChangeConfpassword = (e) => {
+    setConfpassword(e.target.value)
+  }
+
+
   return (
     <div className='container w-25 mt-4'>
         <h3 className='text-center'>Create a new account</h3>
         <form action="" className='mt-4'>
-            <label htmlFor="fname">Firstname</label>
-            <input type="text" className='form-control' />
+            <label htmlFor="fname">{firstname}</label>
+            <input type="text" className='form-control' onChange={handleChangeFirstname} />
             {/* lastname */}
             <label htmlFor="lname">Lastname</label>
-            <input type="text" className='form-control' />
+            <input type="text" className='form-control' onChange={handleChangeLastname} />
 
             <label htmlFor="lname">Email address</label>
             <input type="email" className='form-control' />
